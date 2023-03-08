7d1ccedfb85c62259ee147d7ce55b8b0829afc73
Luis Guillen <luisguillen290301@gmail.com>
2023-03-07 23:59:25 -0500

    Creacion de Login y Usuario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,37 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Devstagram - @yield('titulo') </title>
     @vite('resources/css/app.css')
 </head>
+<body class="bg-gray-100">
+    <header class="p-5 border-b bg-white shadow">
+        
+        <div class="container mx-auto flex justify-between items-center">
+            <h1 class="text-3xl font-black">
+                DevStagram
+            </h1>
 
-<body>
+            <nav class="flex gap-2">
+                <a class="font-bold uppercase text-gray-600 text-sm" href="#"> Login </a>
+                <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta"> Crear Cuenta </a>
+            </nav>
+        </div>
+    </header>
 
-    <h1 class="text-4xl">@yield('titulo')</h1>
-        Devstagram
-    <hr>
+    <main class="container mx-auto mt-10">
+        <h2 class="font-black text-center text-3xl mb-10">
+            @yield('titulo')
+        </h2>
+        @yield('contenido')
+    </main>
 
-    @yield('contenido')
+    <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        DevStagram - Todos los derechos reservados 
+        {{-- Nos permite Imprimir año Actual --}}
+        {{ now()->year }}
+    </footer>
 </body>
 </html>
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,1 +1,11 @@
+{{-- Directivas  --}}
+
 @extends('layouts.app')
+
+@section('titulo')
+Nombre de App - Nombre de Pagina
+@endsection
+
+@section('contenido')
+    Contenido de esta Pagina
+@endsection
